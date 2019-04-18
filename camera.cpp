e86122e7fe0c3fe8b22e548aea084280b14363d7
Aaron Buchwald <aaron.buchwald56@gmail.com>
2019-04-18 12:41:24 -0400

    hope

--- camera.cpp
+++ camera.cpp
@@ -3,8 +3,8 @@
 
 camera::camera() {
     // creates the IIC device to write to later to manipulate multiplexer
-    iviic = IIC((0x70), (0x01));
-    std::cout << "Initializing the camera..." << std::endl;
+    // iviic = IIC((0x70), (0x01));
+    // std::cout << "Initializing the camera..." << std::endl;
 
     // wiringPi abstracts the physical pin numbers
     // pin number counterparts documented here: https://hackage.haskell.org/package/wiringPi
@@ -20,26 +20,33 @@ camera::camera() {
 // return cam number changed to on success
 // returns -1 for an illegal argument
 int camera::set_camera(int cam) {
+    PIN7 = 7;
+    PIN11 = 0;
+    PIN12 = 1;
     if (cam == 1) {
-        iviic.write_control_register((0x01));
+        wiringPiI2CWriteReg8((0x70), (0x00), (0x01));
+        // iviic.write_control_register((0x01));
         digitalWrite(PIN7, LOW);
         digitalWrite(PIN11, LOW);
         digitalWrite(PIN12, HIGH);
 
     } else if (cam == 2) {
-        iviic.write_control_register((0x02));
+        // iviic.write_control_register((0x02));
+        wiringPiI2CWriteReg8((0x70), (0x00), (0x02));
         digitalWrite(PIN7, LOW);
         digitalWrite(PIN11, HIGH);
         digitalWrite(PIN12, HIGH);
 
     } else if (cam == 3) {
-        iviic.write_control_register((0x04));
+        // iviic.write_control_register((0x04));
+        wiringPiI2CWriteReg8((0x70), (0x00), (0x04));
         digitalWrite(PIN7, HIGH);
         digitalWrite(PIN11, LOW);
         digitalWrite(PIN12, LOW);
 
     } else if (cam == 4) {
         // iviic.write_control_register((0x08));
+        // wiringPiI2CWriteReg8((0x70), (0x00), (0x08));
         // assign GPIO pins
 
         std::cout << "No cam 4";
